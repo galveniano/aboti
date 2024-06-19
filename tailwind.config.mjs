8c0336a1c46a68e858609bfb4e49bbda9977bdce
fgrinan <fgrinan@trustportal.org>
2024-06-19 19:06:07 +0200

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,16 +5,23 @@ export default {
 		extend: {
 			colors: {
 				bg1: '#292F36',
+				bg2: '#1A1E23',
 				brand1: '#12F7D6',
+				brand2: '#98FAEC',
 				grey1: '#43454D',
 				negro1: '#1A1E23'		
 			},
 			fontFamily: {
 				code: '"Press Start 2P"',
-				ibm: '"IBM Plex Mono"'
+				ibm: '"IBM Plex Mono"',
+				ubuntu: 'Ubuntu'
 			},
 			boxShadow: {
 				'card': '-4px -4px 2px 0px #12F7D6',
+				'highlights': '2px 2px 4px 0px rgba(0, 0, 0, 0.25)'
+			  },
+			  flex: {
+				'1-0-0': '1 0 0'
 			  },
 		},
 	},
