82d513010249794facba9e69161f038e19070f96
fgrinan <fgrinan@trustportal.org>
2024-06-20 12:34:58 +0200

    wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+import { register } from 'swiper/element/bundle';
+import { keyframes } from '@emotion/react'
 import animations from '@midudev/tailwind-animations'
-
+register()
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -25,9 +27,27 @@ export default {
 			  flex: {
 				'1-0-0': '1 0 0'
 			  },
+			  keyframes: {
+				typing: {
+				  'from': { width: '0' }
+				},
+				blink: {
+				  '50%' :  { borderColor: 'transparent' }
+				},
+			  },
+			  animation: {
+				'typing-20': 'typing 2s steps(20)',
+				blink: 'blink .5s infinite step-end alternate',
+			  },
+			  backgroundImage: {
+				'img-oficina': "url('/img-oficina.webp')"
+			  }
 		},
 	},
 	plugins: [
 		animations
 	],
 }
+
+
+
